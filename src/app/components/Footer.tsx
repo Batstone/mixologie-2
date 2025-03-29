619887ce440b6ff483690efc86841feadc14fb5a
Adam Batstone <59373379+Batstone@users.noreply.github.com>
2025-03-29 15:56:27 +0000

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -5,7 +5,7 @@ import styles from "@/app/styles/Footer.module.css";
 export default function Footer() {
   return (
     <footer className={styles["footer"]}>
-      <p>© 2021 Mixologie</p>
+      <p>© 2025 Mixologie</p>
       <Link href="https://www.adambatstone.dev">
         <span className="sr-only">Visit the portfolio of developer, Adam Batstone. </span>Batstone Development
       </Link>
