90d59ee45301d30771d44523e1e169d6061eb669
Batstone <adam.batstone@gmail.com>
2025-04-02 10:35:19 -0400

    icon update

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,47 +1,52 @@
 import Link from "next/link";
 import Image from "next/image";
+import Head from "next/head"; // Import next/head
 
 import styles from "../styles/Header.module.css";
 
 import { SEARCH, FAVORITES } from "@/constants";
-import { Metadata } from "next";
 
 interface HeaderProps {
   currentPage: string;
 }
 
-export const metadata: Metadata = {
-  title: "Mixologie",
-  description: "Craft the Perfect Cocktail",
-};
-
 export default function Header({ currentPage }: HeaderProps) {
   return (
-    <header className={`${styles.header}`}>
-      <div
-        className={`${styles["header__logo-container"]} ${
-          currentPage === SEARCH ? styles["header__logo-container--home"] : styles["header__logo-container--search"]
-        }`}
-      >
-        <h1>
-          <Image src="/images/logo.png" alt="Logo" fill />
-        </h1>
-      </div>
-      <nav className={styles["header__nav"]}>
-        <ul className={styles["header__nav-list"]}>
-          <li>
-            <Link className={styles["header__nav-link"]} href="/" {...(currentPage === SEARCH ? { "aria-current": "page" } : {})}>
-              Search
-            </Link>
-          </li>
-          <li>
-            <Link className={styles["header__nav-link"]} href="/favorites" {...(currentPage === FAVORITES ? { "aria-current": "page" } : {})}>
-              Favorites
-            </Link>
-          </li>
-          <li></li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <Head>
+        <title>Mixologie | {currentPage === SEARCH ? "Search" : "Favorites"}</title>
+        <meta name="description" content="Craft the Perfect Cocktail" />
+        <link rel="icon" href="../icon.png" />
+        <meta property="og:title" content="Mixologie Cocktail App" />
+        <meta property="og:description" content="Discover and make your favorite cocktails with Mixologie." />
+      </Head>
+
+      <header className={`${styles.header}`}>
+        <div
+          className={`${styles["header__logo-container"]} ${
+            currentPage === SEARCH ? styles["header__logo-container--home"] : styles["header__logo-container--search"]
+          }`}
+        >
+          <h1>
+            <Image src="/images/logo.png" alt="Logo" fill />
+          </h1>
+        </div>
+        <nav className={styles["header__nav"]}>
+          <ul className={styles["header__nav-list"]}>
+            <li>
+              <Link className={styles["header__nav-link"]} href="/" {...(currentPage === SEARCH ? { "aria-current": "page" } : {})}>
+                Search
+              </Link>
+            </li>
+            <li>
+              <Link className={styles["header__nav-link"]} href="/favorites" {...(currentPage === FAVORITES ? { "aria-current": "page" } : {})}>
+                Favorites
+              </Link>
+            </li>
+            <li></li>
+          </ul>
+        </nav>
+      </header>
+    </>
   );
 }
